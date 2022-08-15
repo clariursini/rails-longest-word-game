b690afcda2d95c260ee2fa8930d0c91c1b4ee68a
Clari Ursini <clari_ursini@hotmail.com>
2022-08-15 20:08:03 -0300

    updated style and app

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'json'
 
 class GamesController < ApplicationController
 
@@ -9,17 +10,29 @@ class GamesController < ApplicationController
   def score
     @guess = params[:word]
     @grid = params[:grid]
-    @start_time = Time.new
-    @end_time = Time.new
+    @end_time = Time.now
+    @start_time = params[:start_time]
 
-    if word_in_grid_check(@guess, @grid) && ingles(@guess, @grid) # las letras que use no estan en el el grid
-      @score = "La palabra es válida de acuerdo a la cuadrícula y es una palabra en inglés válida"
-    elsif word_in_grid_check(@guess, @grid)
-      @score = "La palabra es válida pero no es una palabra en ingles válida"
-    else
-      @score = "La palabra no puede crearse a partir de la cuadrícula original"
+    if word_in_grid_check(@guess, @grid) == true
+      if ingles(@guess) == true # word is in english
+        score = get_score(@guess, @start_time, @end_time)
+        @result = { score: score, message: 'well done' }
+      else # word no es en ingles
+        @result = { score: 0, message: 'not an english word' }
+      end
+    else # la palabra no esta en la grilla
+      @result = { score: 0, message: 'not in the grid' }
     end
   end
+
+  #   if word_in_grid_check(@guess, @grid) && ingles(@guess, @grid) # las letras que use no estan en el el grid
+  #     @score = "La palabra es válida de acuerdo a la cuadrícula y es una palabra en inglés válida"
+  #   elsif word_in_grid_check(@guess, @grid)
+  #     @score = "La palabra es válida pero no es una palabra en ingles válida"
+  #   else
+  #     @score = "La palabra no puede crearse a partir de la cuadrícula original"
+  #   end
+  # end
 
   # metodo que verifique si la palabra este en la grilla
   def word_in_grid_check(attempt, grid)
@@ -27,43 +40,21 @@ class GamesController < ApplicationController
   end
 
   # metodo que verifique si es una palabra en ingles (existe la palabra)
-  def ingles(attempt, grid)
-    # API Check
+  def ingles(attempt)
     url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
     user_serialized = URI.open(url).read
     word_hash = JSON.parse(user_serialized)
-    if word_hash["found"] == true
+    if word_hash['found'] == true
       ingles_check = true
     else
       ingles_check = false
     end
-    return ingles_check
+    ingles_check
   end
 
-  # # Calculating the time
-  # def calculate_time(start_time, end_time)
-  #   total_time = end_time - start_time
-  #   return total_time
-  # end
-
   # # metodo que calcule solo score
-  # def get_score(attempt, grid, start_time, end_time)
-  #   score_time = calculate_time(start_time, end_time).to_f
-  #   score = attempt.size.to_f / score_time
-  #   return score
-  # end
-
-    # # recibe el scrore(metodo anterior) y te da el mensaje (llama a los 3 otros metodos)
-    # def run_game(attempt, grid, start_time, end_time)
-    #   if word_in_grid_check(attempt, grid) == true
-    #     if ingles(attempt, grid) == true # word is in english
-    #       score = get_score(attempt, grid, start_time, end_time)
-    #       { time: calculate_time(start_time, end_time), score: score, message: "well done" }
-    #     else # word no es en ingles
-    #       { time: calculate_time(start_time, end_time), score: 0, message: "not an english word" }
-    #     end
-    #   else # la palabra no esta en la grilla
-    #       { time: calculate_time(start_time, end_time), score: 0, message: "not in the grid" }
-    #   end
-    # end
+  def get_score(attempt, start_time, end_time)
+    score_time = (end_time.to_f - start_time.to_f)
+    attempt.size.to_f / score_time
+  end
 end
