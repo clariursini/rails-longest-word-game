5a4e7b4a56a9dd5748202e99b0736a9328725691
Clari Ursini <clari_ursini@hotmail.com>
2022-08-15 18:07:53 -0300

    Finished first part

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "games/new", to: "games#new"
+  post "games/score", to: "games#score"
+
 end
